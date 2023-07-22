64f34017e5da802f953f399f3b346563c951f6b0
Paul Palayur <paulpalayur@gmail.com>
2023-07-22 23:03:26 +1000

    feature:added list components

--- ConnectorManagement/ConnectorManagement.psm1
+++ ConnectorManagement/ConnectorManagement.psm1
@@ -29,3 +29,34 @@ function Get-Connectors {
         }
     }
 }
+
+function Get-Components {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true, HelpMessage='Please provide the Subdomain')]
+        [ValidateNotNullOrEmpty()]
+        [string]$Subdomain,
+
+        [Parameter(Mandatory=$true, HelpMessage='Please provide the OAuth Token')]
+        [ValidateNotNullOrEmpty()]
+        [string]$BearerToken
+    )
+
+    begin {
+        $uri = "https://$($Subdomain)-component_manager.cyberark.cloud/api/components"
+        $header = Get-OAuthHeader -BearerToken $BearerToken
+    }
+
+    process {
+        try {
+            return Invoke-RestMethod -Uri $uri -Method Get -Headers $header
+        }
+        catch {
+            if($_.ErrorDetails.Message) {
+                Write-Error $_.ErrorDetails.Message
+            } else {
+                Write-Error $_
+            }
+        }
+    }
+}
